a4314b94a836e7baaaeea0f4d0f4c19b3059b4e9
enes5759 <enes_gurcu59@hotmail.com>
2015-08-06 14:49:12 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposLauncher
 // @namespace   AposLauncher
-// @include     http://agariopvpserver.com//*
+// @include     http://agariopvpserver.com/*
 // @version     3.063
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
