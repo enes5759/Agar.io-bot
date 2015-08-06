47dbb8166774160ad29b0a5cd38b550b2313371a
enes5759 <enes_gurcu59@hotmail.com>
2015-08-06 14:41:29 +0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agariopvpserver.com/*
 // @version     3.56
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
